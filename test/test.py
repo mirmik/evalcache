43515860d7ed1c911c480ca8c7a42bfa7b48a7c3
mirmik <mirmikns@yandex.ru>
2018-06-25 19:13:29 +0300

    a

--- test/test.py
+++ test/test.py
@@ -7,5 +7,25 @@ sys.path.insert(0, "..")
 import evalcache
 
 @evalcache.lazy
-def a():
-	print ("a")
+def add(a, b):
+	return a + b
+
+evalcache.enable()
+
+class A:
+	def __init__(self, i):
+		self.i = i
+
+	def __add__(self, other):
+		try:
+			return A(self.i + other.i)
+		except:
+			return A(self.i + other)
+
+	def __str__(self):
+		return str(self.i)
+
+a = A(1)
+b = A(2)
+
+print( add(add(a, b), 33).eval() )
