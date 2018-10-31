341fbb4b1bae95a0d5122fc06bebaa7b62a3ce6a
mirmik <mirmikns@yandex.ru>
2018-10-31 11:42:03 +0300

    a

---
+++ utest/start.py
@@ -0,0 +1,143 @@
+#!/usr/bin/python3
+#coding:utf-8
+
+import sys
+sys.path.insert(0, "..")
+
+import unittest
+import test_environment
+
+class TestLazy(unittest.TestCase):
+
+    def tearDown(self):
+        test_environment.clean()
+
+    def test_arithmetic_full(self):  
+        a = test_environment.lazy(1)
+        b = test_environment.lazy(2)
+        c = test_environment.lazy(3)
+        d = test_environment.lazy(4)
+
+        count = (a + b + c).unlazy()
+        total_objects = test_environment.count_cached_objects()
+
+        self.assertEqual(count, 6)
+        self.assertEqual(total_objects, 2) #2 summ
+
+    def test_arithmetic_part(self):
+        a = test_environment.lazy(1)
+        b = test_environment.lazy(2)
+        c = test_environment.lazy(3)
+        d = test_environment.lazy(4)
+
+        count = (a + b + c + d).unlazy()
+        total_objects = test_environment.count_cached_objects()
+
+        self.assertEqual(count, 10)
+        self.assertEqual(total_objects, 3) #3 summ
+
+
+    def test_twoargs(self):  
+        @test_environment.lazy
+        def summ(a, b):
+            return a + b
+
+        summ(0, 0).unlazy()
+        summ(0, 1).unlazy()
+        summ(1, 1).unlazy()
+        summ(1, 1).unlazy()
+        summ(1, b=1).unlazy()
+        summ(a=1, b=1).unlazy()
+        summ(b=1, a=1).unlazy()
+
+        total_objects = test_environment.count_cached_objects()
+        self.assertEqual(total_objects, 5)
+
+    def test_fibonachi(self):
+        @test_environment.lazy
+        def fib(n):
+            if n < 2:
+                return n
+            return fib(n - 1) + fib(n - 2)
+
+        ret = fib(9).unlazy()
+        
+        self.assertEqual(ret, 34)
+        self.assertEqual(test_environment.count_cached_objects(), 18) #range(0-9) and 8 summ
+
+
+class TestMemoize(unittest.TestCase):
+
+    def tearDown(self):
+        test_environment.clean_memoize()
+
+    def test_arithmetic(self):
+
+        @test_environment.memoize
+        def maker(n):
+            return n
+
+        a = test_environment.memoize(1)
+        b = test_environment.memoize(2)
+        c = maker(3)
+        d = maker(4)
+
+        self.assertEqual(len(test_environment.memoize.cache), 0) #no unlazy yet
+
+        count = a + b + c + d
+
+        self.assertEqual(len(test_environment.memoize.cache), 5) #c, d and 3 summ
+        self.assertEqual(count, 10)
+
+
+    def test_fibonachi(self):
+        @test_environment.memoize
+        def fib(n):
+            if n < 2:
+                return n
+            return fib(n - 1) + fib(n - 2)
+
+        ret = fib(9)
+
+        self.assertEqual(ret, 34)
+        self.assertEqual(len(test_environment.memoize.cache), 19) #range(0-9) and 9 summ and eq(assertEqual)
+
+
+class TestOnplaceMemoize(unittest.TestCase):
+
+    def tearDown(self):
+        test_environment.clean_onplace_memoize()
+
+    def test_arithmetic(self):
+
+        @test_environment.onplace_memoize
+        def maker(n):
+            return n
+
+        a = test_environment.onplace_memoize(1)
+        b = test_environment.onplace_memoize(2)
+        c = maker(3)
+        d = maker(4)
+
+        self.assertEqual(len(test_environment.onplace_memoize.cache), 2) #c, d
+
+        count = a + b + c + d
+
+        self.assertEqual(len(test_environment.onplace_memoize.cache), 3) #c, d and a+b
+        self.assertEqual(count, 10)
+
+
+    def test_fibonachi(self):
+
+        @test_environment.onplace_memoize
+        def fib(n):
+            if n < 2:
+                return n
+            return fib(n - 1) + fib(n - 2)
+
+        self.assertEqual(fib(9), 34)
+        self.assertEqual(len(test_environment.onplace_memoize.cache), 10) #range(0-9)
+        
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ utest/test_environment.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+#coding:utf-8
+
+import evalcache
+import evalcache.dircache
+
+import os
+
+dircache_path = ".evalcache"
+
+lazy = evalcache.Lazy(cache = evalcache.dircache.DirCache(dircache_path))
+memoize = evalcache.Memoize(onplace = False)
+onplace_memoize = evalcache.Memoize(onplace = True)
+
+def clean():
+	for path in os.listdir(dircache_path):
+		os.remove("{}/{}".format(dircache_path, path))
+	lazy.cache.files = set() 
+
+def clean_memoize():
+	memoize.cache = {}
+
+def clean_onplace_memoize():
+	onplace_memoize.cache = {}
+
+def count_cached_objects():
+	return len(os.listdir(dircache_path))
