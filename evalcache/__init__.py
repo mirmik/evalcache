598bb97be543a0f1e670df10586f658a05b99fab
mirmik <mirmikns@yandex.ru>
2018-11-17 12:14:37 +0300

    1.4.2

--- evalcache/__init__.py
+++ evalcache/__init__.py
@@ -4,4 +4,4 @@ from evalcache.dircache import DirCache
 from evalcache.lazy import Lazy, LazyObject, LazyHash, Memoize
 from evalcache.lazy import unlazy, encache, decache, print_tree
 
-__version__ = "1.4.1"
+__version__ = "1.4.2"
