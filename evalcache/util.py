3d98432f55ed9a8e7857d94dbc45e5185c1fb371
mirmik <mirmikns@yandex.ru>
2019-02-06 05:37:20 +0300

    'some clean'

--- evalcache/util.py
+++ evalcache/util.py
@@ -1,6 +1,6 @@
 import inspect
 
 def select(obj, func):
-	print(func)
-	print(inspect.getsource(func))
+	#print(func)
+	#print(inspect.getsource(func))
 	return obj.__lazybase__(lambda: [x for x in obj if func(x)], hint=func)()
