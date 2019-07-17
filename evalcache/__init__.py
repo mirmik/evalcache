b2f8dc482a975d0392d4164e5bceee2353281410
mirmik <mirmikns@yandex.ru>
2019-07-17 12:48:27 +0300

    'unlazy_if_need'

--- evalcache/__init__.py
+++ evalcache/__init__.py
@@ -3,6 +3,7 @@
 from evalcache.dircache import DirCache
 from evalcache.lazy import Lazy, LazyObject, LazyHash, Memoize
 from evalcache.lazy import unlazy, encache, decache, nocache, print_tree
+from evalcache.lazy import unlazy_if_need
 from evalcache.lazyfile import LazyFile
 
 from evalcache.util import select
