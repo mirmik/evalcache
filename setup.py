e7d11fe279bfd7beac6892e51d6fb3d9212aaeb3
netricks <mirmikns@yandex.ru>
2025-10-29 20:22:06 +0300

    update version for pypi updating (egg format deprecated)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import evalcache
 
 setup(
     name="evalcache",
-    version="1.14.0",
+    version="1.15.0",
     packages=["evalcache"],
     author="mirmik",
     author_email="mirmikns@yandex.ru",
